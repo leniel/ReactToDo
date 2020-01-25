84cf3fc9c5c3135d6c90c5c9db24a6e19f2fe0f7
Leniel Macaferi <leniel@gmail.com>
2020-01-25 15:25:39 -0300

    - Got todo items displayed from a todo list JSON file. - Used array.map to create a React TodoItem component dynamically avoiding code repetition.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import ToDo from './ToDo';
+import ToDo from './TodoItem';
+import TodoItem from './TodoItem';
+import TodoData from '../data/todosData';
 
 function Main()
 {
+    const todos = TodoData.map(todo => <TodoItem key={todo.id} item={todo} />)
+    
     return (
         <main className="Main">
 
-            <ToDo />
+            <div class="todo-list">
+                {todos}
+            </div>
 
         </main>
     )
